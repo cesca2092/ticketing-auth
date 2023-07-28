a983b36868de79db064ecedfa047daa71b1222b1
cesar <cesar@everst.work>
2023-07-27 20:26:23 -0500

    tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,12 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+// declara una variable global solamente disponible en los tests
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 
@@ -25,4 +31,19 @@ afterAll(async () => {
     await mongo.stop();
   }
   await mongoose.connection.close();
-})
+});
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email, password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+  return cookie;
+}
